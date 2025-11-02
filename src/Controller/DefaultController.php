eaca8c307bcc2d8ffb08a6937eb00090924fc889
Litvin <Dmitry.Litvin@mvideo.ru>
2025-11-02 06:33:56 +0300

    123

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Repository\FilterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): Response
+    #[Route('/', name: 'app_default')]
+    public function index(FilterRepository $repository): Response
     {
+        $m = $repository->findBy(['parent' => null]);
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'm' => $m,
+        ]);
+    }
+
+    #[Route('/booking/{id}', name: 'app_booking')]
+    public function books(Request $request,FilterRepository $repository): Response
+    {
+        if(!$request->get('id')) throw $this->createNotFoundException();
+
+        $m = $repository->findBy(['parent' => $request->get('id')]);
+
+        return $this->render('default/booking.html.twig', [
+            'm' => $m,
+        ]);
+    }
+
+    #[Route('/booking2/{id}', name: 'app_booking2')]
+    public function books2(Request $request,FilterRepository $repository): Response
+    {
+        if(!$request->get('id')) throw $this->createNotFoundException();
+
+        $m = $repository->findBy(['parent' => $request->get('id')]);
+
+        return $this->render('default/booking2.html.twig', [
+            'm' => $m,
         ]);
     }
 }
